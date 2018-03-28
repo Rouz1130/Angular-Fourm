c0aedf99133bfb304a96b015a77560c4e241b58e
rouzbeh majlessi <rmajlessi@gmail.com>
2018-03-28 15:30:31 -0700

    Add Input decorator to article component , iterate over artilce of articles to display list of articles called from root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,14 @@ import { Article } from './article/article.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  addArticle (title: HTMLInputElement, link: HTMLInputElement) : boolean {
-    console.log(`Adding article title: ${title.value} add link: ${link.value}`);
-    title.value = '';
-    link.value = '';
-    return false;
-  }
+  articles: Article[]; // <-- component property
 
+  constructor() {
+    this.articles = [
+      new Article('Air Jordan 1', 'https://www.flightclub.com/air-jordan-1-retro-high-og-white-black-varsity-red-012234', 3),
+      new Article('Air Jordan 2', 'https://www.flightclub.com/air-jordan-2-retro-bg-gs-white-black-vrsty-red-cmnt-gry-011940', 2),
+      new Article('Air Jordan 3', 'https://www.goat.com/sneakers/air-jordan-3-retro-og-true-blue-2016-854262-106?gclid=Cj0KCQjw-uzVBRDkARIsALkZAdk8dDTJMOtujPKULhIs6OmF_SmXEdS-jnXjhi14la8_IRJORrDkEkQaAiOwEALw_wcB', 1),
+
+    ];
+  }
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding ,Input} from '@angular/core';
 import { Article } from './article.model';
 
 
@@ -15,13 +15,11 @@ import { Article } from './article.model';
 //It encapsulates the selector tag within our markup, meaning we dont have to use it the selector tag in the parent component as well
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article;
+  @Input() article: Article; 
+
 
   constructor() {
-    this.article = new Article(
-      'Angular 2',
-      'http://angular.io',
-      10);
+   
   }
 
 
