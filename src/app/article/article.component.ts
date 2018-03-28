45f3d4e16e9e3d9ce1d3fc2a1dbacaa2e4754ced
rouzbeh majlessi <rmajlessi@gmail.com>
2018-03-28 13:26:19 -0700

    Add up and down vote fucntions to increent and decrement in corresponding component file, Add HostBinding to encapsulate element selector tag to article component

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { HostBinding } from '@angular/core/src/metadata/directives';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit {
 
-  constructor() { }
+// @HostBiding: element <app-article> this component is attached to
+//This exmaple telling Angular to keep the value of the host elements class to be in sync wiht property cssClass
+//by using this @Host.., we set class attribute to have a row. 
+//It encapsulates the selector tag within our markup, meaning we dont have to use it the selector tag in the parent component as well
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass='row';
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor() { 
+    this.title = 'Angular 5';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+
+  // increment vote by 1
+  voteUp() {
+    this.votes += 1;
+  }
+
+
+  // decrement vote by 1
+  voteDown() {
+    this.votes -= 1;
+  }
+  
 
   ngOnInit() {
   }
